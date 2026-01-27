95abc2da94080fd84f0a54013bb92794aaded60f
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-01-27 13:17:55 -0300

    Add files via upload

---
+++ alura_pandas.py
@@ -0,0 +1,64 @@
+import pandas
+
+data = pandas.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
+#print(data.columns) #Check names of columns
+#print(data["remote_ratio"].value_counts()) #Test
+
+experience_level = {
+    "SE": "Senior",
+    "MI": "Mid-level",
+    "EN": "Junior",
+    "EX": "Executive"
+}
+data["experience_level"] = data["experience_level"].replace(experience_level)
+
+employment_type = {
+    "FT": "Full-time",
+    "PT": "Part-time",
+    "FL": "Freelance",
+    "CT": "Contract"
+}
+data["employment_type"] = data["employment_type"].replace(employment_type)
+
+company_size = {
+    "S": "Small",
+    "M": "Medium",
+    "L": "Large"
+}
+data["company_size"] = data["company_size"].replace(company_size)
+
+remote_ratio = {
+    0: "On-site",
+    50: "Hybrid",
+    100: "Remote"
+}
+data["remote_ratio"] = data["remote_ratio"].replace(remote_ratio)
+
+def data_verification():
+    while True:
+        try:
+            i = int(input("Option: "))
+            if 6 >= i >= 1:
+                return i
+            raise ValueError
+        except ValueError:
+            print("Invalid value")
+
+while True: #Main menu
+    print("\n1- Show experience level \n2- Show employment type \n3- Show company size \n4- Show remote ratio")
+    print("5- Show data count \n6- Finish program")
+    option = data_verification()
+
+    if option == 1: #Experience level
+        print("\n", data["experience_level"].value_counts())
+    elif option == 2: #Employment type
+        print("\n", data["employment_type"].value_counts())
+    elif option == 3: #Company size
+        print("\n", data["company_size"].value_counts())
+    elif option == 4: #Remote ration
+        print("\n", data["remote_ratio"].value_counts())
+    elif option == 5:
+        print("\n", data.describe(include = "object"))
+    elif option == 6:
+        print("Ending program...")
+        break
